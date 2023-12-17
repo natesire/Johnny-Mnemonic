427c339c1f097bb1c077c175592a5ea11c1c7e7d
Nate Sire <nate.sire@fairwaymc.com>
2023-12-17 00:02:05 -0500

    cannot use toString in the hash key

--- JSRay.js
+++ JSRay.js
@@ -1,65 +1,53 @@
-//let allMethods = Object.getOwnPropertyNames(Array.prototype);
-//console.log(allMethods.sort());
+let allMethods = Object.getOwnPropertyNames(Array.prototype);
+console.log(allMethods.sort());
 
-var allMethodsHash = {};
+var triplesMethods = {};
 
 let jsArr = [1,[2,3],3,4];
 
-function check(name, callFunc, arg1, arg2) {
-    console.log(name, callFunc, arg1, arg2);
-    allMethodsHash[name] || (allMethodsHash[name] = new Array())
-    allMethodsHash[name].push(callFunc); 
-    jsArr[callFunc](arg1);
+function ray(memoryGroup, callFunc, arg1, arg2) {
+    try {
+      triplesMethods[memoryGroup] || (triplesMethods[memoryGroup] = new Array());
+      triplesMethods[memoryGroup].push(callFunc);
+      jsArr[callFunc](arg1);
+    } catch (e) {
+      console.log(memoryGroup, callFunc, arg1, e.message);
+    }
 }
 
-check("at", "at", 1);
-check("flat", "flat");
-check("flat", "map", function() { 1 });
-check("flat", "flatMap", function(item) { return item + 1; });
+ray("at", "at", 9725);
+ray("flat", "flat");
+ray("flat", "map", function() { 1 });
+ray("flat", "flatMap", function(item) { return item + 1; });
+ray("forEach", "forEach", function(item) { return item + 1; });
+ray("forEach", "find", function(item) { item === 3 });
+ray("forEach", "filter", function(item) { item === 3 });
+ray("pop", "pop", function(item) { item === 3 });
+ray("pop", "splice", function(item) { item === 3 });
+ray("pop", "slice", function(item) { item === 3 });
+ray("push", "push", function(item) { item === 3 });
+ray("push", "unshift", function(item) { item === 3 });
+ray("push", "concat", function(cat) { cat === 3 });
+ray("shift", "shift", function(item) { item === 3 });
+ray("shift", "reduce", function(cats) { cats === 3 });
+ray("shift", "reduceRight", function(item) { item === 3 });
+ray("some", "some", function(get) { get === 3 });
+ray("some", "includes", function(nude) { nude === 3 });
+ray("some", "every", function(every) { every === 3 });
+ray("sort", "fill", function(every) { every === 3 }); //most difficult to remember goes first
+ray("sort", "reverse", function(every) { every === 3 });
+ray("sort", "sort", function(every) { every === 3 }); // easiest to remember goes last
 
+// cannot use toString as the key name
+// triplesMethods[memoryGroup].push is not a function
+ray("toString2", "toLocaleString", function(every) { every === 3 }); // easiest to remember goes last
+ray("toString2", "join", function(every) { every === 3 }); // easiest to remember goes last
+ray("toString2", "toString"); // easiest to remember goes last
 
-/*
+ray("values", "values", function(every) { every === 3 }); // easiest to remember goes last
+ray("values", "entries", function(every) { every === 3 }); // easiest to remember goes last
+ray("values", "keys", function(every) { every === 3 }); // easiest to remember goes last
 
-// flatMap, flat, map
-let flat = jsArr.flat();
-console.log(flat);
-
-let flattened = jsArr.flatMap(function(item){
-    return item + 1;
-})
-console.log(flattened);
-
-let map = flat.map(function(item){
-    return item + 2;
-})
-console.log(map);
-
-allMethodsHash["flat"] = [ "flat", "map", "flatMap" ];
-
-// forEach, find 3, filter
-jsArr.forEach(function(item){
-    console.log(item);
-});
-
-let find = jsArr.find(function(item) {
-    return(item == 3);
-});
-console.log("find: " + find);
-
-// findIndex, indexOf, lastIndexOf
-let findIndex = jsArr.findIndex(function(item) {
-    return(item == 3);
-}); 
-
-indexOf = jsArr.indexOf(findIndex);
-
-allMethodsHash["findIndex"] = [ "findIndex", "indexOf" ];
-
-
-*/
-
-// print all
-Object.keys(allMethodsHash).forEach(function (key) { 
-    console.log("allMethods");
-    console.log(key, allMethodsHash[key]);
+Object.keys(triplesMethods).forEach(function (key) { 
+    console.log(key, triplesMethods[key]);
 });

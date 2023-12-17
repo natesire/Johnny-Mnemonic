1b121650275495acadf5d62d64f4607ac6fff149
Nate Sire <nate.sire@fairwaymc.com>
2023-12-17 02:43:48 -0500

    adding Python version

---
+++ Python/main.py
@@ -0,0 +1,30 @@
+
+class Mipi:
+    def __init__(self):
+        self.name = "MyPi"
+        self.age = 20
+
+    def say_hi(self):
+        print("Hi, my name is", self.name)
+
+    def say_age(self):
+        print("I am", self.age, "years old")
+
+    def say_hi_and_age(self):
+        self.say_hi()
+        self.say_age()
+
+def Str(str, call):
+    print(getattr(str, call)())
+
+str = "Hello MyPi"
+dir(str)
+
+# instantiate class
+mipi = Mipi()
+
+print(dir(mipi))
+
+# Haiku
+Str("haiku", { "name": [ "title", "capitalize" ] })
+Str("haiku", "capitalize")

e66e06c3645be85a06da77794bd7047dfd2066eb
Nate Sire <nate.sire@fairwaymc.com>
2023-12-18 02:49:50 -0500

    the is functions don't take arguments

--- Python/main.py
+++ Python/main.py
@@ -6,15 +6,49 @@ def Str(str, call, arg = None):
 
 str = "Hello MyPy"
 funcs = dir(str)
+print(funcs)
 for f in funcs:
     # search for substr
-    if f.find("find") > -1:
+    if f.find("is") > -1:
         print(f)
 
-#Str("haiku", { "group": [ "title", "capitalize" ] })
 Str("group1", "capitalize")
 Str("group1", "casefold")
 Str("group1", "center", 1)
 
 Str("find", "find", "a")
 Str("find", "rfind", "a")
+
+Str("format", "format", "a")
+Str("format", "format_map", "a")
+
+Str("index", "index", "i")
+Str("index", "rindex", "i")
+
+Str("is", "isalnum")
+Str("is", "isalpha")
+Str("is", "isascii")
+Str("is", "isdecimal")
+Str("is", "isdigit")
+Str("is", "isidentifier")
+Str("is", "islower")
+Str("is", "isnumeric")
+Str("is", "isprintable")
+Str("is", "isspace")
+Str("is", "istitle")
+Str("is", "isupper")
+
+Str("upper", "isupper")
+
+Str("strip", "strip")
+Str("strip", "rstrip")
+Str("strip", "lstrip")
+
+Str("split", "split")
+Str("split", "rsplit")
+Str("split", "splitlines")
+
+Str("endswith", "endswith", "a")
+Str("startsWith", "splitlines")
+
+Str("zfill", "zfill", 1)

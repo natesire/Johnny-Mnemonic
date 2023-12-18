8cca8a044b545611cd68cc6e099c7f16075b3aa4
Nate Sire <nate.sire@fairwaymc.com>
2023-12-18 00:49:31 -0500

    write a helper function to search for keywords in the function list

--- Python/main.py
+++ Python/main.py
@@ -5,9 +5,16 @@ def Str(str, call, arg = None):
         print(getattr(str, call)())
 
 str = "Hello MyPy"
-#print(dir(str))
+funcs = dir(str)
+for f in funcs:
+    # search for substr
+    if f.find("find") > -1:
+        print(f)
 
 #Str("haiku", { "group": [ "title", "capitalize" ] })
 Str("group1", "capitalize")
 Str("group1", "casefold")
 Str("group1", "center", 1)
+
+Str("find", "find", "a")
+Str("find", "rfind", "a")
